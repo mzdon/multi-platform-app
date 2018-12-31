0af3a118f3b3af36cee80ba6c7e095fc34b7cc0a
Marc Zdon <marczdon@gmail.com>
2018-12-31 09:53:17 -0500

    Update haul.config.js so we don't have to make manual changes to files in node_modules/haul

--- mobile/haul.config.js
+++ mobile/haul.config.js
@@ -1,3 +1,44 @@
-export default {
-    entry: "./index.js"
+/**
+ * @param options
+ *      type Options = {
+ *          platform: 'ios' | 'android', // Current platform
+ *          dev: boolean, // Whether to build for development
+ *          minify: boolean, // Whether to minify the bundle
+ *          bundle: boolean, // Whether building the bundle for packaging
+ *          root: string, // Absolute path to the project root
+ *      }
+ * @param defaults - default config used by Haul... this is what I want to extend
+ */
+
+module.exports = (options, defaults) => {
+    const ruleIndex = defaults.module.rules.findIndex(r => {
+        return r.test && r.test.toString().indexOf("js$");
+    });
+
+    console.log("Rule Index: " + ruleIndex);
+
+    let config;
+    if (ruleIndex) {
+        config = {
+            ...defaults,
+            entry: "./index.js",
+            module: {
+                ...defaults.module,
+                rules: [
+                    ...defaults.module.rules,
+                    {
+                        ...defaults.module.rules[ruleIndex],
+                        test: /\.jsx?$/,
+                        exclude: /node_modules(?!.*[\/\\](react|@react-navigation|@expo|pretty-format|haul|metro))/
+                    }
+                ],
+            }
+        };
+    } else {
+        config = defaults;
+    }
+
+    console.log(config);
+
+    return config;
 };
